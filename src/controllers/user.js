0fc021d1169197ac8cbc1914402ec6c935a27b00
giga-mania <hil.giga@gmail.com>
2023-06-23 14:48:22 +0100

    Add registerUser controller: handle errors, check for user duplications

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,12 +1,69 @@
-const registerUser = (req, res) => {
-    const {firstName, lastName, userName, email, password, passwordRepeat} = req.body
+import {PrismaClient} from "@prisma/client";
+import {hashPassword} from "../utils/utils.js";
 
-    console.log(req.body)
-    res.json(req.body)
+const prisma = new PrismaClient()
+
+const registerUser = async (req, res) => {
+    const {firstName, lastName, username, email, password, passwordRepeat} = req.body
+
     try {
+        if (password !== passwordRepeat) {
+            return res.status(400).json({
+                status: "FAILED",
+                data: {
+                    error: "Provided password don't match"
+                }
+            })
+        }
 
-    } catch (e) {
+        const emailUser = await prisma.user.findUnique({
+            where: {
+                email: email
+            }
+        })
+        if (emailUser){
+            return res.status(400).json({
+                status: 'FAILED',
+                data: {
+                    error: 'User with this email already exists!'
+                }
+            })
+        }
 
+        const usernameUser = await prisma.user.findUnique({
+            where: {
+                username: username
+            }
+        })
+        if (usernameUser) {
+            return res.status(400).json({
+                status: 'FAILED',
+                data: {
+                    error: 'User with this username already exists!'
+                }
+            })
+        }
+
+        const hashedPassword = await hashPassword(password)
+
+        const newUser = await prisma.user.create({
+            data: {
+                firstName: firstName,
+                lastName: lastName,
+                username: username,
+                email: email,
+                password: hashedPassword
+            }
+        })
+
+        res.status(201).json({
+            status: 'OK',
+            data: newUser
+        })
+    } catch (err) {
+        res
+            .status(err?.status || 500)
+            .send({status: "FAILED", data: {error: err.message || err}})
     }
 }
 
