ce998868c5965c87fae7422fb40769d761b5aca1
giga-mania <hil.giga@gmail.com>
2023-06-24 16:23:14 +0100

    Implement GET authorBooks service: add pagination

--- src/controllers/author.js
+++ src/controllers/author.js
@@ -6,7 +6,7 @@ const prisma = new PrismaClient()
 
 const getAuthorList = async (req, res) => {
     const {page} = req.query
-    const recordsToSkip = page && page !== "1" ? page * 10 : 0
+    const recordsToSkip = page && page !== "1" ? (Number(page) - 1) * 10: 0
 
 
     try {
@@ -16,7 +16,7 @@ const getAuthorList = async (req, res) => {
             take: 10
         })
 
-        const nextPageNum = authors.length < 10 ? null : !page ? 1 : Number(page) + 1
+        const nextPageNum = page && authorCount > page * 10 ? Number(page) + 1 : null
         const prevPageNum = page && page !== "1" && page !== "0" ? Number(page) - 1 : null
 
         const nextPageURL = nextPageNum ? `${req.protocol}://${req.get('host')}${req.baseUrl}/?page=${nextPageNum}` : null
@@ -37,6 +37,7 @@ const getAuthorList = async (req, res) => {
             .send({status: "FAILED", data: {error: err?.message || err}})
     }
 }
+
 
 const getSingleAuthor = async (req, res) => {
     const {authorId} = req.params
@@ -69,7 +70,47 @@ const getSingleAuthor = async (req, res) => {
 }
 
 const getAuthorBooks = async (req, res) => {
+    const {authorId} = req.params
+    const {page} = req.query
+    const recordsToSkip = page && page !== "1" ? (Number(page) - 1) * 10: 0
 
+
+    try {
+        const authorBookCount = await prisma.book.count({
+            where: {
+                authorId: authorId
+            }
+        })
+
+        const authorBooks = await prisma.book.findMany({
+            skip: recordsToSkip,
+            take: 10,
+            where: {
+                authorId: authorId
+            }
+        })
+
+        const nextPageNum = page && authorBookCount > page * 10 ? Number(page) + 1 : null
+        const prevPageNum = page && page !== "1" && page !== "0" ? Number(page) - 1 : null
+        
+        const nextPageURL = nextPageNum ? `${req.protocol}://${req.get('host')}${req.baseUrl}/?page=${nextPageNum}` : null
+        const prevPageURL = prevPageNum ? `${req.protocol}://${req.get('host')}${req.baseUrl}/?page=${prevPageNum}` : null
+
+
+        res.status(200).json({
+            status: "OK",
+            data: {
+                count: authorBookCount,
+                next: nextPageURL,
+                prev: prevPageURL,
+                results: authorBooks
+            }
+        })
+    } catch (err) {
+        res
+            .status(err?.status || 500)
+            .send({status: "FAILED", data: {error: err?.message || err}})
+    }
 }
 
 
