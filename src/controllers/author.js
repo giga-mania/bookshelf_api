e61feb643f41aa7c26e384c912b23bf2088b1462
giga-mania <hil.giga@gmail.com>
2023-06-24 15:21:44 +0100

    Implement GET authorList service

--- src/controllers/author.js
+++ src/controllers/author.js
@@ -1,5 +1,41 @@
-const getAuthorList = async (req, res) => {
+import {PrismaClient} from "@prisma/client";
 
+
+const prisma = new PrismaClient()
+
+
+const getAuthorList = async (req, res) => {
+    const {page} = req.query
+    const recordsToSkip = page && page !== "1" ? page * 10 : 0
+
+
+    try {
+        const authorCount = await prisma.author.count()
+        const authors = await prisma.book.findMany({
+            skip: recordsToSkip,
+            take: 10
+        })
+
+        const nextPageNum = authors.length < 10 ? null : !page ? 1 : Number(page) + 1
+        const prevPageNum = page && page !== "1" && page !== "0" ? Number(page) - 1 : null
+
+        const nextPageURL = nextPageNum ? `${req.protocol}://${req.get('host')}${req.baseUrl}/?page=${nextPageNum}` : null
+        const prevPageURL = prevPageNum ? `${req.protocol}://${req.get('host')}${req.baseUrl}/?page=${prevPageNum}` : null
+
+        res.status(200).json({
+            status: "OK",
+            data: {
+                count: authorCount,
+                next: nextPageURL,
+                prev: prevPageURL,
+                results: authors
+            }
+        })
+    } catch (err) {
+        res
+            .status(err?.status || 500)
+            .send({status: "FAILED", data: {error: err?.message || err}})
+    }
 }
 
 const getSingleAuthor = async (req, res) => {
