16176a9cd4d0d35834485da88c63f3a5f1e1bd28
giga-mania <hil.giga@gmail.com>
2023-06-26 14:22:44 +0100

    Add service logic to createEvent controller and handle invalid requests

--- src/controllers/event.js
+++ src/controllers/event.js
@@ -1,9 +1,80 @@
-const getEvent = async (req, res) => {
+import {PrismaClient} from "@prisma/client";
+import jwt from "jsonwebtoken";
 
+
+const prisma = new PrismaClient()
+
+const getEvent = async (req, res) => {
+    try {
+
+    } catch (err) {
+
+    }
 }
 
 const createEvent = async (req, res) => {
+    const {bookId} = req.params
+    const {id: userId} = jwt.decode(req.cookies.jwt)
+    const {eventDate, title, city, byInvitation, ageRegulation} = req.body
 
+
+    try {
+        const book = await prisma.book.findUnique({
+            where: {
+                id: bookId
+            }
+        })
+
+        if (!book) {
+            return res.status(400).json({
+                status: 'FAILED',
+                data: {
+                    error: 'Instance of a book with given id does not exist!'
+                }
+            })
+        }
+
+
+        const existingEvent = await prisma.event.findMany({
+            where: {
+                bookId,
+                userId
+            }
+        })
+
+        if(existingEvent.length > 0) {
+            return res.status(400).json({
+                status: 'FAILED',
+                data: {
+                    error: 'Event for this book already exists!'
+                }
+            })
+        }
+        console.log("reaches the skyyy")
+
+        const newEvent = await prisma.event.create({
+            data: {
+                title,
+                city,
+                eventDate: new Date(eventDate),
+                byInvitation,
+                ageRegulation,
+                bookId,
+                userId
+            }
+        })
+
+
+        res.status(200).json({
+            status: "OK",
+            data: newEvent
+        })
+    } catch (err) {
+        console.log(err)
+        res
+            .status(err?.status || 500)
+            .send({status: "FAILED", data: {error: err?.message || err}})
+    }
 }
 
 const updateEvent = async (req, res) => {
