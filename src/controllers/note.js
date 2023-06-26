8716b2b2ef03262bff084a3a10df1a73f74ab86a
giga-mania <hil.giga@gmail.com>
2023-06-26 12:41:42 +0100

    Add createNote logic: handle note creation, and bad requests with invalid parameteres

--- src/controllers/note.js
+++ src/controllers/note.js
@@ -1,13 +1,85 @@
-const createNote = async (req, res) => {
+import {PrismaClient} from "@prisma/client";
+import jwt from "jsonwebtoken";
 
+
+const prisma = new PrismaClient()
+
+const createNote = async (req, res) => {
+    const {text} = req.body
+    const {bookId} = req.params
+    const {id: userId} = jwt.verify(req.cookies.jwt, process.env.JWT_SECRET_KEY)
+
+    try {
+        const book = await prisma.book.findUnique({
+            where: {
+                id: bookId
+            }
+        })
+
+        if(!book) {
+            return res.status(400).json({
+                status: 'FAILED',
+                data: {
+                    error: 'Instance of a book with given id does not exist!'
+                }
+            })
+        }
+
+
+        const existingNote = await prisma.note.findMany({
+            where: {
+                bookId,
+                userId
+            }
+        })
+
+        if (existingNote.length) {
+            return res.status(400).json({
+                status: 'FAILED',
+                data: {
+                    error: 'Note for this book already exists!'
+                }
+            })
+        }
+
+
+        const note = await prisma.note.create({
+            data: {
+                bookId,
+                userId,
+                text
+            },
+        })
+
+        res.status(200).json({
+            status: 'OK',
+            data: {
+                id: note.id,
+                text: note.text
+            }
+        })
+    } catch (err) {
+        res
+            .status(err?.status || 500)
+            .send({status: "FAILED", data: {error: err?.message || err}})
+    }
 }
 
 const updateNote = async (req, res) => {
 
+    try {
+
+    } catch (err) {
+
+    }
 }
 
 const deleteNote = async (req, res) => {
+    try {
 
+    } catch (err) {
+
+    }
 }
 
 
