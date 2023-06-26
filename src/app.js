3de6f9950460efdcf52df3348a62ad17b523717f
giga-mania <hil.giga@gmail.com>
2023-06-26 11:54:40 +0100

    Create note router for POST, PUT, and DELETE notes

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser"
 import userRouter from "./routes/user.js"
 import bookRouter from  "./routes/book.js"
 import authorRouter from "./routes/author.js"
+import noteRouter from "./routes/note.js"
 
 const app = express()
 
@@ -15,6 +16,7 @@ app.use(cookieParser())
 app.use('/api/user', userRouter)
 app.use('/api/book', bookRouter)
 app.use('/api/author', authorRouter)
+app.use('/api/note', noteRouter)
 
 
 export default app
---
+++ src/routes/note.js
@@ -0,0 +1,12 @@
+import {Router} from "express";
+
+
+const router = Router()
+
+router.post('book/:bookId', )
+
+router.put('/:nodeId', )
+
+router.delete('/:nodeId', )
+
+export default router
