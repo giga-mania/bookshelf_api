1a22bc3029dd6338932b01878d7dde6cf32f1e9e
giga-mania <hil.giga@gmail.com>
2023-06-26 13:29:55 +0100

    Create event router for GET, POST, PATCH, DELETE events

---
+++ src/routes/event.js
@@ -0,0 +1,12 @@
+import {Router} from "express";
+
+
+const router = Router()
+
+router.get('/:eventId',)
+router.post('/book/:bookId')
+router.patch('/:eventId')
+router.delete('/:eventId')
+
+
+export default router
