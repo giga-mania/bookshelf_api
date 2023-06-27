12a7fc950323a29f495c468c349ab3a9dd881b0a
giga-mania <hil.giga@gmail.com>
2023-06-27 20:54:30 +0100

    Create authenticateToken middleware: check and verify a token in the Authorization header

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import express from "express"
 import cookieParser from "cookie-parser"
 import userRouter from "./routes/user.js"
-import bookRouter from  "./routes/book.js"
+import bookRouter from "./routes/book.js"
 import authorRouter from "./routes/author.js"
 import noteRouter from "./routes/note.js"
 import eventRouter from "./routes/event.js"
+import authenticateToken from "./middleware/authenticateToken.js";
 
 const app = express()
 
@@ -15,6 +16,7 @@ app.use(cookieParser())
 
 
 app.use('/api/user', userRouter)
+app.use(authenticateToken)
 app.use('/api/book', bookRouter)
 app.use('/api/author', authorRouter)
 app.use('/api/note', noteRouter)
---
+++ src/middleware/authenticateToken.js
@@ -0,0 +1,46 @@
+import jwt from "jsonwebtoken"
+
+
+const catchError = (err, res) => {
+    const {TokenExpiredError} = jwt
+    if(err instanceof TokenExpiredError) {
+        return res.status(401).json({
+            status: 'FAILED',
+            data: {
+                error: 'Unauthorized! Access token is expired!'
+            }
+        })
+    }
+
+    return res.status(401).json({
+        status: 'FAILED',
+        data: {
+            error: `Unauthorized: ${err?.message}`
+        }
+    })
+}
+
+const authenticateToken = (req, res, next) => {
+    const token = req.headers.authorization.slice(7);
+
+    if(!token) {
+        return res.status(400).json({
+            status: 'FAILED',
+            data: {
+                error: 'Token were not provided!'
+            }
+        })
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
+        if(err) {
+            return catchError(err, res)
+        }
+
+        req.userId = decoded.id
+        next()
+    })
+}
+
+
+export default authenticateToken
