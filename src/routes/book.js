e5671dfac8e65ee06cebc109186fdb14e51a612e
giga-mania <hil.giga@gmail.com>
2023-06-28 16:03:33 +0100

    Fix bug: Rearange /tag /:bookId routes

--- src/routes/book.js
+++ src/routes/book.js
@@ -5,8 +5,8 @@ import {getSingeBook, getBookList, getBookTags} from "../controllers/book.js"
 const router = Router()
 
 router.get('/', getBookList)
-router.get('/:bookId', getSingeBook)
 router.get('/tags', getBookTags)
+router.get('/:bookId', getSingeBook)
 
 
 export default router
