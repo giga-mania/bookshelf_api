b918b3965a4a3e6fc03ca2b226dadc52749e3580
giga-mania <hil.giga@gmail.com>
2023-06-28 15:42:06 +0100

    Populate db seeding data with addiotal required dataset

--- src/prisma/seed.js
+++ src/prisma/seed.js
@@ -3,7 +3,57 @@ import {PrismaClient} from "@prisma/client";
 const prisma = new PrismaClient()
 
 async function main() {
-    prisma.book.createMany()
+    const tags = await prisma.tag.createMany({
+        data: [
+            {
+                name: "Adventure Stories",
+            },
+            {
+                name: "Classics",
+            },
+            {
+                name: "Crime",
+            },
+            {
+                name: "Fairy Tales",
+            },
+            {
+                name: "Fantasy",
+            },
+            {
+                name: "Historical Fiction",
+            },
+            {
+                name: "Horror",
+            },
+            {
+                name: "Humour and Satire",
+            }
+        ]
+    })
+
+    const book_tag = await prisma.book.create({
+        data: {
+            title: "Book of tag",
+            description: "Book of tag book of tag book of tag",
+            publishDate: new Date(2000, 3, 20),
+            language: "Finnish",
+            author: {
+                create: {
+                    firstName: "Brit",
+                    lastName: "Lagos",
+                    birthDate: new Date(1997, 2, 18)
+                },
+            },
+            tag: {
+                connect: [
+                    {name: "Adventure Stories"},
+                    {name: "Classics"}
+                ],
+            }
+        }
+    })
+
     const book_a = await prisma.book.upsert({
         where: {id: "bc"},
         update: {},
@@ -17,9 +67,15 @@ async function main() {
                     firstName: "Brit",
                     lastName: "Lagos",
                     birthDate: new Date(1997, 2, 18)
-                }
+                },
+            },
+            tag: {
+                connect: [
+                    {name: "Fairy Tales"},
+                    {name: "Fantasy"}
+                ]
             }
-        },
+        }
     })
 
     const book_b = await prisma.book.upsert({
@@ -36,6 +92,12 @@ async function main() {
                     lastName: "Moreno",
                     birthDate: new Date(1987, 9, 24)
                 }
+            },
+            tag: {
+                connect: [
+                    {name: "Horror"},
+                    {name: "Humour and Satire"}
+                ]
             }
         },
     })
@@ -54,6 +116,11 @@ async function main() {
                     lastName: 'Smith',
                     birthDate: new Date(1980, 8, 2)
                 }
+            },
+            tag: {
+                connect: [
+                    {name: "Crime"}
+                ]
             }
         }
     })
@@ -72,6 +139,12 @@ async function main() {
                     lastName: 'Garcia',
                     birthDate: new Date(1975, 3, 14)
                 }
+            },
+            tag: {
+                connect: [
+                    {name: "Fantasy"},
+                    {name: "Classics"}
+                ]
             }
         }
     })
@@ -90,6 +163,12 @@ async function main() {
                     lastName: 'Müller',
                     birthDate: new Date(1989, 10, 30)
                 }
+            },
+            tag: {
+                connect: [
+                    {name: "Fairy Tales"},
+                    {name: "Historical Fiction"},
+                ]
             }
         }
     })
@@ -108,6 +187,11 @@ async function main() {
                     lastName: 'Dupont',
                     birthDate: new Date(1985, 4, 7)
                 }
+            },
+            tag: {
+                connect: [
+                    {name: "Humour and Satire"}
+                ]
             }
         }
     })
@@ -126,6 +210,11 @@ async function main() {
                     lastName: 'Ricci',
                     birthDate: new Date(1992, 7, 21)
                 }
+            },
+            tag: {
+                connect: [
+                    {name: "Adventure Stories"}
+                ]
             }
         }
     })
@@ -144,6 +233,12 @@ async function main() {
                     lastName: 'Janssen',
                     birthDate: new Date(1982, 9, 10)
                 }
+            },
+            tag: {
+                connect: [
+                    {name: "Crime"},
+                    {name: "Fantasy"}
+                ]
             }
         }
     })
@@ -162,6 +257,12 @@ async function main() {
                     lastName: 'Andersson',
                     birthDate: new Date(1986, 3, 16)
                 }
+            },
+            tag: {
+                connect: [
+                    {name: "Fantasy"},
+                    {name: "Humour and Satire"},
+                ]
             }
         }
     })
@@ -180,6 +281,12 @@ async function main() {
                     lastName: 'Santos',
                     birthDate: new Date(1990, 6, 25)
                 }
+            },
+            tag: {
+                connect: [
+                    {name: "Fantasy"},
+                    {name: "Fairy Tales"},
+                ]
             }
         }
     })
@@ -198,6 +305,12 @@ async function main() {
                     lastName: 'Johnson',
                     birthDate: new Date(1983, 6, 5)
                 }
+            },
+            tag: {
+                connect: [
+                    {name: "Historical Fiction"},
+                    {name: "Classics"},
+                ]
             }
         }
     })
@@ -216,6 +329,12 @@ async function main() {
                     lastName: 'Lopez',
                     birthDate: new Date(1979, 10, 18)
                 }
+            },
+            tag: {
+                connect: [
+                    {name: "Crime"},
+                    {name: "Humour and Satire"},
+                ]
             }
         }
     })
@@ -234,6 +353,12 @@ async function main() {
                     lastName: 'Schmidt',
                     birthDate: new Date(1992, 1, 23)
                 }
+            },
+            tag: {
+                connect: [
+                    {name: "Fantasy"},
+                    {name: "Classics"},
+                ]
             }
         }
     })
@@ -252,6 +377,12 @@ async function main() {
                     lastName: 'Martin',
                     birthDate: new Date(1988, 3, 9)
                 }
+            },
+            tag: {
+                connect: [
+                    {name: "Fairy Tales"},
+                    {name: "Adventure Stories"},
+                ]
             }
         }
     })
@@ -270,6 +401,11 @@ async function main() {
                     lastName: 'Rossi',
                     birthDate: new Date(1995, 9, 14)
                 }
+            },
+            tag: {
+                connect: [
+                    {name: "Horror"}
+                ]
             }
         }
     })
@@ -288,6 +424,11 @@ async function main() {
                     lastName: 'De Vries',
                     birthDate: new Date(1984, 2, 28)
                 }
+            },
+            tag: {
+                connect: [
+                    {name: "Adventure Stories"}
+                ]
             }
         }
     })
@@ -306,6 +447,12 @@ async function main() {
                     lastName: 'Lindberg',
                     birthDate: new Date(1987, 7, 3)
                 }
+            },
+            tag: {
+                connect: [
+                    {name: "Fantasy"},
+                    {name: "Crime"},
+                ]
             }
         }
     })
@@ -324,6 +471,12 @@ async function main() {
                     lastName: 'Silva',
                     birthDate: new Date(1991, 4, 11)
                 }
+            },
+            tag: {
+                connect: [
+                    {name: "Classics"},
+                    {name: "Fantasy"},
+                ]
             }
         }
     })
@@ -342,6 +495,11 @@ async function main() {
                     lastName: 'Ivanov',
                     birthDate: new Date(1981, 8, 27)
                 }
+            },
+            tag: {
+                connect: [
+                    {name: "Classics"}
+                ]
             }
         }
     })
@@ -360,10 +518,14 @@ async function main() {
                     lastName: 'Chen',
                     birthDate: new Date(1986, 12, 3)
                 }
+            },
+            tag: {
+                connect: [
+                    {name: "Fantasy"}
+                ]
             }
         }
     })
-
 
 
     const author_a = await prisma.author.upsert({
@@ -373,127 +535,244 @@ async function main() {
             firstName: "Kennedy",
             lastName: "Wembley",
             birthDate: new Date(2004, 12, 31),
-            Book: {
+            book: {
                 create: [
                     {
                         title: 'Book of Mouse',
                         description: 'Book of Mouse Book of Mouse Book of Mouse Book of Mouse Book of Mouse',
                         publishDate: new Date(2009, 7, 12),
                         language: 'Norwegian',
+                        tag: {
+                            connect: {name: "Fantasy"}
+                        }
                     },
                     {
                         title: 'Book of Giraffe',
                         description: 'Book of Giraffe Book of Giraffe Book of Giraffe Book of Giraffe Book of Giraffe',
                         publishDate: new Date(2011, 6, 2),
                         language: 'Caribbean',
+                        tag: {
+                            connect: [
+                                {name: "Fantasy"},
+                                {name: "Fairy Tales"}
+                            ]
+                        }
                     },
                     {
                         title: 'Book of Elephant',
                         description: 'Book of Elephant Book of Elephant Book of Elephant Book of Elephant',
                         publishDate: new Date(2009, 1, 27),
                         language: 'Portuguese',
+                        tag: {
+                            connect: [
+                                {name: "Crime"},
+                                {name: "Adventure Stories"}
+                            ]
+                        }
                     },
                     {
                         title: 'Book of Lion',
                         description: 'Book of Lion Book of Lion Book of Lion Book of Lion Book of Lion',
                         publishDate: new Date(2015, 3, 8),
                         language: 'English',
+                        tag: {
+                            connect: [
+                                {name: "Fantasy"},
+                                {name: "Fairy Tales"}
+                            ]
+                        }
                     },
                     {
                         title: 'Book of Snake',
                         description: 'Book of Snake Book of Snake Book of Snake Book of Snake Book of Snake',
                         publishDate: new Date(2012, 9, 20),
                         language: 'Spanish',
+                        tag: {
+                            connect: [
+                                {name: "Fantasy"},
+                                {name: "Fairy Tales"}
+                            ]
+                        }
                     },
                     {
                         title: 'Book of Bear',
                         description: 'Book of Bear Book of Bear Book of Bear Book of Bear Book of Bear',
                         publishDate: new Date(2013, 11, 5),
                         language: 'German',
+                        tag: {
+                            connect: [
+                                {name: "Fantasy"},
+                                {name: "Fairy Tales"}
+                            ]
+                        }
                     },
                     {
                         title: 'Book of Tiger',
                         description: 'Book of Tiger Book of Tiger Book of Tiger Book of Tiger Book of Tiger',
                         publishDate: new Date(2010, 4, 15),
                         language: 'French',
+                        tag: {
+                            connect: [
+                                {name: "Fantasy"},
+                                {name: "Fairy Tales"}
+                            ]
+                        }
                     },
                     {
                         title: 'Book of Dolphin',
                         description: 'Book of Dolphin Book of Dolphin Book of Dolphin Book of Dolphin Book of Dolphin',
                         publishDate: new Date(2008, 2, 10),
                         language: 'Italian',
+                        tag: {
+                            connect: [
+                                {name: "Fantasy"},
+                                {name: "Fairy Tales"}
+                            ]
+                        }
                     },
                     {
                         title: 'Book of Owl',
                         description: 'Book of Owl Book of Owl Book of Owl Book of Owl Book of Owl',
                         publishDate: new Date(2014, 8, 25),
                         language: 'Dutch',
+                        tag: {
+                            connect: [
+                                {name: "Fantasy"},
+                                {name: "Fairy Tales"}
+                            ]
+                        }
                     },
                     {
                         title: 'Book of Monkey',
                         description: 'Book of Monkey Book of Monkey Book of Monkey Book of Monkey Book of Monkey',
                         publishDate: new Date(2007, 6, 18),
                         language: 'Swedish',
+                        tag: {
+                            connect: [
+                                {name: "Fantasy"},
+                                {name: "Fairy Tales"}
+                            ]
+                        }
                     },
                     {
                         title: 'Book of Penguin',
                         description: 'Book of Penguin Book of Penguin Book of Penguin Book of Penguin Book of Penguin',
                         publishDate: new Date(2019, 1, 3),
                         language: 'Portuguese',
+                        tag: {
+                            connect: [
+                                {name: "Fantasy"},
+                                {name: "Fairy Tales"}
+                            ]
+                        }
                     },
                     {
                         title: 'Book of Wolf',
                         description: 'Book of Wolf Book of Wolf Book of Wolf Book of Wolf Book of Wolf',
                         publishDate: new Date(2016, 11, 30),
                         language: 'Russian',
+                        tag: {
+                            connect: [
+                                {name: "Fantasy"},
+                                {name: "Fairy Tales"}
+                            ]
+                        }
                     },
                     {
                         title: 'Book of Fox',
                         description: 'Book of Fox Book of Fox Book of Fox Book of Fox Book of Fox',
                         publishDate: new Date(2005, 5, 7),
                         language: 'Japanese',
+                        tag: {
+                            connect: [
+                                {name: "Fantasy"},
+                                {name: "Fairy Tales"}
+                            ]
+                        }
                     },
                     {
                         title: 'Book of Horse',
                         description: 'Book of Horse Book of Horse Book of Horse Book of Horse Book of Horse',
                         publishDate: new Date(2018, 3, 22),
                         language: 'Korean',
+                        tag: {
+                            connect: [
+                                {name: "Fantasy"},
+                                {name: "Fairy Tales"}
+                            ]
+                        }
                     },
                     {
                         title: 'Book of Eagle',
                         description: 'Book of Eagle Book of Eagle Book of Eagle Book of Eagle Book of Eagle',
                         publishDate: new Date(2017, 7, 14),
                         language: 'Chinese',
+                        tag: {
+                            connect: [
+                                {name: "Fantasy"},
+                                {name: "Fairy Tales"}
+                            ]
+                        }
                     },
                     {
                         title: 'Book of Shark',
                         description: 'Book of Shark Book of Shark Book of Shark Book of Shark Book of Shark',
                         publishDate: new Date(2022, 9, 1),
                         language: 'Arabic',
+                        tag: {
+                            connect: [
+                                {name: "Fantasy"},
+                                {name: "Fairy Tales"}
+                            ]
+                        }
                     },
                     {
                         title: 'Book of Koala',
                         description: 'Book of Koala Book of Koala Book of Koala Book of Koala Book of Koala',
                         publishDate: new Date(2021, 10, 11),
                         language: 'Greek',
+                        tag: {
+                            connect: [
+                                {name: "Fantasy"},
+                                {name: "Fairy Tales"}
+                            ]
+                        }
                     },
                     {
                         title: 'Book of Crocodile',
                         description: 'Book of Crocodile Book of Crocodile Book of Crocodile Book of Crocodile',
                         publishDate: new Date(2019, 12, 6),
                         language: 'Spanish',
+                        tag: {
+                            connect: [
+                                {name: "Fantasy"},
+                                {name: "Fairy Tales"}
+                            ]
+                        }
                     },
                     {
                         title: 'Book of Zebra',
                         description: 'Book of Zebra Book of Zebra Book of Zebra Book of Zebra Book of Zebra',
                         publishDate: new Date(2020, 2, 18),
                         language: 'German',
+                        tag: {
+                            connect: [
+                                {name: "Fantasy"},
+                                {name: "Fairy Tales"}
+                            ]
+                        }
                     },
                     {
                         title: 'Book of Penguin',
                         description: 'Book of Penguin Book of Penguin Book of Penguin Book of Penguin Book of Penguin',
                         publishDate: new Date(2016, 8, 9),
                         language: 'French',
+                        tag: {
+                            connect: [
+                                {name: "Fantasy"},
+                                {name: "Fairy Tales"}
+                            ]
+                        }
                     }
                 ]
             }
