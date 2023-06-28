6f8aa076eb8ded5457c129ff3b56df8d71900727
giga-mania <hil.giga@gmail.com>
2023-06-28 15:47:11 +0100

    Add route for GET bookTags

--- src/routes/book.js
+++ src/routes/book.js
@@ -1,11 +1,12 @@
 import {Router} from  "express"
-import {getSingeBook, getBookList} from "../controllers/book.js"
+import {getSingeBook, getBookList, getBookTags} from "../controllers/book.js"
 
 
 const router = Router()
 
 router.get('/', getBookList)
 router.get('/:bookId', getSingeBook)
+router.get('/tags', getBookTags)
 
 
 export default router
