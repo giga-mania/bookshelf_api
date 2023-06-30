28599f71ec3b538273372604944d03a399fb352f
giga-mania <hil.giga@gmail.com>
2023-06-30 09:54:58 +0100

    Refactor user login, register and token refresh logic into separate controller and service

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,32 +1,9 @@
-import {PrismaClient} from "@prisma/client";
-import jwt from "jsonwebtoken"
-import {createToken, hashPassword, verifyPassword} from "../utils/utils.js";
+import userService from "../services/user.service.js"
 
-const prisma = new PrismaClient()
 
 const registerUser = async (req, res) => {
-    const {firstName, lastName, username, email, password, passwordRepeat} = req.body
-
     try {
-        if (password !== passwordRepeat) {
-            return res.status(400).json({
-                status: "FAILED",
-                data: {
-                    error: "Provided password don't match"
-                }
-            })
-        }
-
-        const hashedPassword = await hashPassword(password)
-        const newUser = await prisma.user.create({
-            data: {
-                firstName: firstName,
-                lastName: lastName,
-                username: username,
-                email: email,
-                password: hashedPassword
-            }
-        })
+        const newUser = await userService.registerUser(req.body)
 
         res.status(201).json({
             status: 'OK',
@@ -35,60 +12,14 @@ const registerUser = async (req, res) => {
     } catch (err) {
         res
             .status(err?.status || 500)
-            .send({status: "FAILED", data: {error: err?.message || err}})
+            .send({status: "FAILED", data: {error: err.message}})
     }
 }
 
 
 const loginUser = async (req, res) => {
-    const {username, password} = req.body
-
-    if (!username || !password) {
-        return res.status(400).json({
-            status: 'FAILED',
-            data: {
-                error: 'Credentials were not provided!'
-            }
-        })
-    }
-
     try {
-        const user = await prisma.user.findUnique({
-            where: {
-                username: username
-            }
-        })
-
-        if (!user) {
-            return res.status(401).json({
-                status: 'FAILED',
-                data: {
-                    error: "User with these credential doesn't exist!"
-                }
-            })
-        }
-
-        const isPasswordValid = await verifyPassword(password, user.password)
-
-        if (!isPasswordValid) {
-            return res.status(401).json({
-                status: 'FAILED',
-                data: {
-                    error: "User with these credential doesn't exist!"
-                }
-            })
-        }
-
-        const userInfo = {
-            id: user.id,
-            firstName: user.firstName,
-            lastName: user.lastName,
-            username: user.username,
-            email: user.email,
-            isSuperUser: user.isSuperUser
-        }
-        const accessToken = createToken(userInfo, '15m')
-        const refreshToken = createToken(userInfo, '1d')
+        const {accessToken, refreshToken} = await userService.loginsUser(req.body)
 
         res.cookie('jwt', refreshToken, {
             httpOnly: true,
@@ -125,16 +56,7 @@ const refreshToken = (req, res) => {
     }
 
     try {
-        const decoded = jwt.decode(refreshToken)
-        const userInfo = {
-            id: decoded.id,
-            firstName: decoded.firstName,
-            lastName: decoded.lastName,
-            email: decoded.email,
-            username: decoded.username,
-            isSuperUser: decoded.isSuperUser,
-        }
-        const accessToken = createToken(userInfo, '15m')
+        const accessToken = userService.refreshToken(refreshToken)
 
         res.status(200).json({
             status: "OK",
---
+++ src/services/user.service.js
@@ -0,0 +1,99 @@
+import {PrismaClient} from "@prisma/client"
+import {createToken, hashPassword, verifyPassword} from "../utils/utils.js";
+import jwt from "jsonwebtoken";
+
+
+const prisma = new PrismaClient()
+
+const registerUser = async ({firstName, lastName, username, email, password, passwordRepeat}) => {
+    if (password !== passwordRepeat) {
+        throw {
+            status: 400,
+            message: 'Provided password don\'t match'
+        }
+    }
+
+    const hashedPassword = await hashPassword(password)
+    return prisma.user.create({
+        data: {
+            firstName: firstName,
+            lastName: lastName,
+            username: username,
+            email: email,
+            password: hashedPassword
+        }
+    })
+}
+
+
+const loginsUser = async ({username, password}) => {
+    if (!username || !password) {
+        throw {
+            status: 400,
+            message: 'Credentials were not provided!'
+        }
+    }
+
+    const user = await prisma.user.findUnique({
+        where: {
+            username: username
+        }
+    })
+
+
+    if (!user) {
+        throw {
+            status: 404,
+            message: "User with these credentials doesn't exist!"
+        }
+    }
+
+    const isPasswordValid = await verifyPassword(password, user.password)
+
+    if (!isPasswordValid) {
+        throw {
+            status: 404,
+            message: "User with these credentials doesn't exist!"
+        }
+    }
+
+    const userInfo = {
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        username: user.username,
+        email: user.email,
+        isSuperUser: user.isSuperUser
+    }
+    const accessToken = createToken(userInfo, '15m')
+    const refreshToken = createToken(userInfo, '1d')
+
+    return {
+        accessToken,
+        refreshToken
+    }
+}
+
+
+const refreshToken = (token) => {
+    const decoded = jwt.decode(token)
+    const userInfo = {
+        id: decoded.id,
+        firstName: decoded.firstName,
+        lastName: decoded.lastName,
+        email: decoded.email,
+        username: decoded.username,
+        isSuperUser: decoded.isSuperUser,
+    }
+
+    return createToken(userInfo, '15m')
+}
+
+
+const userService = {
+    registerUser,
+    loginsUser,
+    refreshToken
+}
+
+export default userService
