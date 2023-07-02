0bcce35e94519ef2671622fd4c0a64abc5cf0535
giga-mania <hil.giga@gmail.com>
2023-07-02 18:13:44 +0100

    Refactor authenticateTokenMiddleware error handling to use custom error objects.

--- src/middlewares/authenticateToken.middleware.js
+++ src/middlewares/authenticateToken.middleware.js
@@ -1,40 +1,26 @@
 import jwt from "jsonwebtoken"
+import Api401Error from "../errors/api401.error.js";
 
 
-const catchError = (err, res) => {
+const catchError = (err) => {
     const {TokenExpiredError} = jwt
     if(err instanceof TokenExpiredError) {
-        return res.status(401).json({
-            status: 'FAILED',
-            data: {
-                error: 'Unauthorized! Access token is expired!'
-            }
-        })
+        throw new Api401Error('Unauthorized! Access token is expired!')
     }
 
-    return res.status(401).json({
-        status: 'FAILED',
-        data: {
-            error: `Unauthorized: ${err?.message}`
-        }
-    })
+    throw new Api401Error(`Unauthorized: ${err?.message}`)
 }
 
 const authenticateTokenMiddleware = (req, res, next) => {
     const token = req.headers.authorization?.slice(7);
 
     if(!token) {
-        return res.status(400).json({
-            status: 'FAILED',
-            data: {
-                error: 'Token were not provided!'
-            }
-        })
+        throw new Api401Error('Unauthorized! Token were not provided!')
     }
 
     jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
         if(err) {
-            return catchError(err, res)
+            return catchError(err)
         }
 
         req.userId = decoded.id
