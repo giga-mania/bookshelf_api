0a9a91a037d0046f706bf6dab5f70d7e97428ac3
giga-mania <hil.giga@gmail.com>
2023-07-02 18:46:37 +0100

    Refactor checkDuplicateUserCredentialsMiddleware split up into helper functions

--- src/middlewares/checkDuplicateUserCredentials.middleware.js
+++ src/middlewares/checkDuplicateUserCredentials.middleware.js
@@ -4,28 +4,22 @@ import Api400Error from "../errors/api400.error.js";
 
 const prisma = new PrismaClient()
 
+const checkByEmail = async (email) => {
+    const emailUser = await prisma.user.findUnique({where: {email: email}})
+    if (emailUser) throw new Api400Error('User with this email already exists!')
+}
+
+const checkByUsername = async (username) => {
+    const usernameUser = await prisma.user.findUnique({where: {username: username}})
+    if (usernameUser) throw new Api400Error('User with this username already exists!')
+}
+
 const checkDuplicateUserCredentialsMiddleware = async (req, res, next) => {
     const {username, email} = req.body
 
     try {
-        const emailUser = await prisma.user.findUnique({
-            where: {
-                email: email
-            }
-        })
-        if (emailUser) {
-            throw new Api400Error('User with this email already exists!')
-        }
-
-
-        const usernameUser = await prisma.user.findUnique({
-            where: {
-                username: username
-            }
-        })
-        if (usernameUser) {
-            throw new Api400Error('User with this username already exists!')
-        }
+        await checkByEmail(email)
+        await checkByUsername(username)
 
         next()
     } catch (err) {
